45948a54f0ac0700d4433f0c09a845516c6f8f55
Leif <leifl@kth.se>
2022-09-28 16:41:42 +0200

    fill without depending on pk values

--- ceo-data.sql
+++ ceo-data.sql
@@ -1,5 +1,7 @@
 INSERT INTO employee (id,name,department_id,managers_employee_id)
-VALUES
-  ('HYA984230948','Richard Jones',2,NULL);
+   VALUES ('HYA984230948','Richard Jones',
+           (select department_id from department where name = 'admin'),
+	   NULL
+	  ); 
 
 
