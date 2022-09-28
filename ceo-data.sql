d3fbcdceca944fee103c47b030522f74e52ac8d7
Leif <leifl@kth.se>
2022-09-28 16:49:25 +0200

    deleted obsolete files

--- ceo-data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO employee (id,name,department_id,managers_employee_id)
-   VALUES ('HYA984230948','Richard Jones',
-           (select department_id from department where name = 'admin'),
-	   NULL
-	  ); 
-
-
