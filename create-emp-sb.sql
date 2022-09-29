c4c13082e42fac94e09e98d1d7301d01d722006f
Leif <leifl@kth.se>
2022-09-29 14:56:01 +0200

    added comments

--- create-emp-sb.sql
+++ create-emp-sb.sql
@@ -1,3 +1,4 @@
+-- Department
 CREATE TABLE department (
  department_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
  name VARCHAR(100)
@@ -5,7 +6,7 @@ CREATE TABLE department (
 
 ALTER TABLE department ADD CONSTRAINT PK_department PRIMARY KEY (department_id);
 
-
+-- Employee
 CREATE TABLE employee (
  employee_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
  id VARCHAR(100) NOT NULL,
@@ -16,7 +17,7 @@ CREATE TABLE employee (
 
 ALTER TABLE employee ADD CONSTRAINT PK_employee PRIMARY KEY (employee_id);
 
-
+-- Laptop
 CREATE TABLE laptop (
  laptop_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
  id VARCHAR(100) NOT NULL,
@@ -27,7 +28,7 @@ CREATE TABLE laptop (
 
 ALTER TABLE laptop ADD CONSTRAINT PK_laptop PRIMARY KEY (laptop_id);
 
-
+-- Phone
 CREATE TABLE phone (
  phone_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
  id VARCHAR(100) NOT NULL,
@@ -37,7 +38,7 @@ CREATE TABLE phone (
 
 ALTER TABLE phone ADD CONSTRAINT PK_phone PRIMARY KEY (phone_id);
 
-
+-- Role
 CREATE TABLE role (
  role_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
  title VARCHAR(100),
@@ -46,7 +47,7 @@ CREATE TABLE role (
 
 ALTER TABLE role ADD CONSTRAINT PK_role PRIMARY KEY (role_id);
 
-
+-- Employee-role cross reference table
 CREATE TABLE employee_role (
  employee_id INT NOT NULL,
  role_id INT NOT NULL
@@ -54,17 +55,14 @@ CREATE TABLE employee_role (
 
 ALTER TABLE employee_role ADD CONSTRAINT PK_employee_role PRIMARY KEY (employee_id,role_id);
 
-
+-- Foreign keys
 ALTER TABLE employee ADD CONSTRAINT FK_employee_0 FOREIGN KEY (department_id) REFERENCES department (department_id);
 ALTER TABLE employee ADD CONSTRAINT FK_employee_1 FOREIGN KEY (managers_employee_id) REFERENCES employee (employee_id);
-
 
 ALTER TABLE laptop ADD CONSTRAINT FK_laptop_0 FOREIGN KEY (employee_id) REFERENCES employee (employee_id);
 ALTER TABLE laptop ADD CONSTRAINT FK_laptop_1 FOREIGN KEY (department_id) REFERENCES department (department_id);
 
-
 ALTER TABLE phone ADD CONSTRAINT FK_phone_0 FOREIGN KEY (employee_id) REFERENCES employee (employee_id);
-
 
 ALTER TABLE employee_role ADD CONSTRAINT FK_employee_role_0 FOREIGN KEY (employee_id) REFERENCES employee (employee_id);
 ALTER TABLE employee_role ADD CONSTRAINT FK_employee_role_1 FOREIGN KEY (role_id) REFERENCES role (role_id);
